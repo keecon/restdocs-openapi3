4d32be5a117bb335b503cad86b8f350038c159ad
iwaltgen <iwaltgen@gmail.com>
2026-08-27 00:36:23 +0900

    test: replace JUnit Pioneer with Jupiter TempDir

--- restdocs-api-spec-gradle-plugin/src/test/kotlin/com/keecon/restdocs/apispec/gradle/RestdocsOpenApi3TaskTest.kt
+++ restdocs-api-spec-gradle-plugin/src/test/kotlin/com/keecon/restdocs/apispec/gradle/RestdocsOpenApi3TaskTest.kt
@@ -3,11 +3,8 @@ package com.keecon.restdocs.apispec.gradle
 import com.jayway.jsonpath.JsonPath
 import org.assertj.core.api.BDDAssertions.then
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.junitpioneer.jupiter.TempDirectory
 import java.lang.Boolean.FALSE
 
-@ExtendWith(TempDirectory::class)
 class RestdocsOpenApi3TaskTest : RestdocsOpenApiTaskTestBase() {
 
     override val taskName = "openapi3"
