3811755ab71d8c6f523e3fbacb73c988a2ec0e85
iwaltgen <iwaltgen@gmail.com>
2026-09-08 13:01:42 +0900

    fix: retain Java 17 target for Groovy test compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,6 +94,8 @@ allprojects {
     }
 
     java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(25))
         }
